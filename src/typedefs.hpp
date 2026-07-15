e49f64d4518647db2d0920be29e158f6bb6c86f8
Mikhail Fomichev <mialfomichev@edu.hse.ru>
2026-07-15 22:04:21 +0300

    small changes

--- src/typedefs.hpp
+++ src/typedefs.hpp
@@ -4,8 +4,7 @@
 
 namespace borophene {
 
-static_assert(std::endian::native == std::endian::little,
-              "That engine supports only Little-Endian architectures");
+static_assert(std::endian::native == std::endian::little, "That engine supports only Little-Endian architectures");
 
 using idx_t = uint64_t;
 
